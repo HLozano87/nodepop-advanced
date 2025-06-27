535841140de915331dddd9f4ca958d611d693e7d
Hector Lozano <hectorlg87@gmail.com>
2025-06-27 23:07:53 +0200

    feat: improve validation of parameters in product creation

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,13 @@
 import { body, validationResult } from "express-validator";
 import Product from "../models/Product.js";
 import User from "../models/User.js";
-import path from "node:path"
+import path from "node:path";
 import { unlink } from "node:fs/promises";
 
 export const validateParams = [
   body("name")
-    .matches(/^([a-zA-Z]+|\w+\d)(\s([a-zA-Z]+|\w+\d))*$/)
+    .trim()
+    .matches(/^[a-zA-Z0-9 áéíóúÁÉÍÓÚñÑ]+$/)
     .isString()
     .withMessage(
       "El nombre del producto es obligatorio, y debe ser valido. Ej: Producto 1 o Producto."
@@ -14,16 +15,21 @@ export const validateParams = [
   body("price")
     .custom((value) => value > 0)
     .withMessage("El precio debe ser un número mayor a 0"),
-  body("image")
-    .optional()
-    .isString()
-    .withMessage("La URL de la imagen debe ser válida"),
-  body("tags")
-    .isArray()
-    .withMessage("Las etiquetas deben ser un arreglo de strings"),
-  (req, res, next) => {
+  body("image").optional().isString(),
+  body("tags").isArray().withMessage("Las etiquetas deben ser un arreglo"),
+  body("tags.*").isString().withMessage("Cada etiqueta debe ser un string"),
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      if (req.file) {
+        const image = req.file.filename;
+        const imagePath = path.join(process.cwd(), "public", "uploads", image);
+        try {
+          await unlink(imagePath);
+        } catch (error) {
+          console.error("Remove image", error);
+        }
+      }
       return next(errors);
     }
     next();
@@ -49,6 +55,8 @@ export const createProduct = async (req, res, next) => {
     });
     await product.save();
 
+    //TODO TOAST
+
     res.redirect("/");
   } catch (error) {
     next(error);
@@ -63,17 +71,18 @@ export const deleteProduct = async (req, res, next) => {
     const user = await User.findById(userId);
 
     if (product.image) {
-      const imagePath = path.join(process.cwd(), "public", "uploads", product.image)
+      const imagePath = path.join(
+        process.cwd(),
+        "public",
+        "uploads",
+        product.image
+      );
       await unlink(imagePath);
     }
-    
+
     await Product.deleteOne({ _id: productId, owner: userId });
-    
-    // Send email transactional
-    user.sendEmail(
-      `You have successfully deleted ${product.name}`,
-      `Hello ${user.name} you have successfully deleted ${product.name}.`
-    );
+
+    // TODO TOAST
 
     res.redirect("/");
   } catch (error) {
