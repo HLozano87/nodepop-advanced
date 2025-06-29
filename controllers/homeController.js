f03cd4ce27d4fd1af4878d23c712d4d4d1e23cdc
Hector Lozano <hectorlg87@gmail.com>
2025-06-29 12:57:10 +0200

    fix: improve pagination logic

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -9,27 +9,29 @@ export const index = async (req, res, next) => {
     const filterTags = req.query.tags;
     const sort = req.query.sort;
 
-    const page = parseInt(req.query.page) || 1;
+    let page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     const filter = {
       owner: userId,
     };
-
+    
     if (filterName) {
       filter.name = filterName.trim().toLowerCase();
     }
     if (filterPrice) {
       filter.price = filterPrice;
     }
-
+    
     if (filterTags) {
       filter.tags = filterTags;
     }
-
+    
     const totalProducts = await Product.countDocuments(filter);
     const totalPages = Math.ceil(totalProducts / limit);
+
+    if (page < 1 || page > totalPages) return next()
 
     const products = await Product.list(filter, limit, skip, sort);
     const uniqueTags = await Product.distinct("tags");
